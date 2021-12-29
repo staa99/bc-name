3cf6c6ef8d0d7497ad0445ed7a823cd5ec782400
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2021-12-29 01:07:05 +0100

    Implement search result card

--- dapp/src/components/SearchResultCard.tsx
+++ dapp/src/components/SearchResultCard.tsx
@@ -3,98 +3,72 @@ import {
   Button,
   Center,
   Heading,
-  Link,
   Stack,
   Text,
   useColorModeValue,
   VStack,
 } from '@chakra-ui/react'
+import { SearchResult } from '../types/SearchResult'
 
-const SearchResultCard = () => {
+interface SearchResultCardProps {
+  searchResult: SearchResult
+  selectedAddress?: string
+}
+
+const SearchResultCard = ({
+  searchResult,
+  selectedAddress,
+}: SearchResultCardProps) => {
+  const colorMode = useColorModeValue('gray.50', 'gray.800')
   return (
     <Center py={6}>
       <VStack justifyContent="center" alignItems="center" p={1} pt={2}>
-        <Heading fontSize={'2xl'} fontFamily={'body'}>
-          Lindsey James
+        <Heading fontSize={'md'} fontFamily={'body'}>
+          {searchResult.address}
         </Heading>
         <Text fontWeight={600} color={'gray.500'} size="sm" mb={4}>
-          @lindsey_jam3s
+          {searchResult.address !== selectedAddress
+            ? 'All names owned'
+            : 'Your linked names'}
         </Text>
-        <Text
-          textAlign={'center'}
-          color={useColorModeValue('gray.700', 'gray.400')}
-          px={3}
-        >
-          Actress, musician, songwriter and artist. PM for work inquires or
-          <Link href={'#'} color={'blue.400'}>
-            #tag
-          </Link>
-          me in your posts
-        </Text>
+
         <Stack align={'center'} justify={'center'} direction={'row'} mt={6}>
-          <Badge
-            px={2}
-            py={1}
-            bg={useColorModeValue('gray.50', 'gray.800')}
-            fontWeight={'400'}
-          >
-            #art
-          </Badge>
-          <Badge
-            px={2}
-            py={1}
-            bg={useColorModeValue('gray.50', 'gray.800')}
-            fontWeight={'400'}
-          >
-            #photography
-          </Badge>
-          <Badge
-            px={2}
-            py={1}
-            bg={useColorModeValue('gray.50', 'gray.800')}
-            fontWeight={'400'}
-          >
-            #music
-          </Badge>
+          {searchResult.ownedNames?.map((n) => (
+            <Badge px={2} py={1} bg={colorMode} fontWeight={'400'} key={n}>
+              {n}
+            </Badge>
+          ))}
         </Stack>
 
-        <Stack
-          width={'100%'}
-          mt={'2rem'}
-          direction={'row'}
-          padding={2}
-          justifyContent={'space-between'}
-          alignItems={'center'}
-        >
-          <Button
-            flex={1}
-            fontSize={'sm'}
-            rounded={'full'}
-            _focus={{
-              bg: 'gray.200',
-            }}
+        {searchResult.address === selectedAddress && (
+          <Stack
+            width={'100%'}
+            mt={'2rem'}
+            direction={'row'}
+            padding={2}
+            justifyContent={'space-around'}
+            alignItems={'center'}
           >
-            Message
-          </Button>
-          <Button
-            flex={1}
-            fontSize={'sm'}
-            rounded={'full'}
-            bg={'blue.400'}
-            color={'white'}
-            boxShadow={
-              '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-            }
-            _hover={{
-              bg: 'blue.500',
-            }}
-            _focus={{
-              bg: 'blue.500',
-            }}
-          >
-            Follow
-          </Button>
-        </Stack>
+            <Button
+              width={'50%'}
+              fontSize={'sm'}
+              rounded={'md'}
+              bg={'blue.400'}
+              color={'white'}
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'blue.500',
+              }}
+              _focus={{
+                bg: 'blue.500',
+              }}
+            >
+              Transfer
+            </Button>
+          </Stack>
+        )}
       </VStack>
     </Center>
   )
